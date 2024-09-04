18233884f6fd130dc8965fd14dbad25188b22568
nukestarmaster <69655738+nukestarmaster@users.noreply.github.com>
2024-09-04 10:18:30 -0700

    Got the player triangle rendering

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import pygame
+import player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    p1 = player.player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         screen.fill("black")
+        p1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
