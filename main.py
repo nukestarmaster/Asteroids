3adde004f5fe6473f094abaa68a4e5fd9b92b55f
nukestarmaster <nukestarmaster@gmail.com>
2024-09-06 09:55:28 -0700

    implement player movement

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import pygame
 import player
 from constants import *
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     p1 = player.player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatable.add(p1)
+    drawable.add(p1)
     while True:
         screen.fill("black")
-        p1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        for s in drawable:
+            s.draw(screen)
+        updatable.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
