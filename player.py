12f04c90de3c094c595bda2ebf5dc9a56a705d86
nukestarmaster <69655738+nukestarmaster@users.noreply.github.com>
2024-09-09 10:22:51 -0700

    impelment shot cooldown

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
         player.containers = (updatable, drawable)
         updatable.add(self)
         drawable.add(self)
@@ -34,6 +35,7 @@ class player(circleshape.CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
         self.velocity *= 1 - PLAYER_FRICTION * dt
+        self.cooldown -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -44,8 +46,10 @@ class player(circleshape.CircleShape):
         if keys[pygame.K_s]:
             self.accelerate(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.cooldown <= 0:
+                self.shoot()
+                self.cooldown = PLAYER_SHOOT_COOLDOWN
 
     def shoot(self):
-        velocity = pygame.Vector2(0, 1).rotate(self.rotation)*SHOT_SPEED
+        velocity = pygame.Vector2(0, 1).rotate(self.rotation)*SHOT_SPEED + self.velocity
         new_shot = shot.shot(self.position.x, self.position.y, velocity)
